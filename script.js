f15264505407c82a90f3b093d0286af3e037abeb
NiteshShivam <niteshyadavand2000nitesh@gmail.com>
2024-05-20 14:48:38 +0530

    script.js update

--- script.js
+++ script.js
@@ -5,15 +5,26 @@ let audioElement = new Audio("songs/1.mp3");
 let masterPlay = document.getElementById('masterPlay');
 let myProgessBar = document.getElementById('myProgressBar');
 let gif = document.getElementById('gif');
+let songItems = Array.from(document.getElementsByClassName('songImtem'));
 
 let songs = [
     {songName:"Salam-e-Ishq",filePath:"song/1.mp3",coverPath:"covers/1.jpg"},
-    {songName:"Salam-e-Ishq",filePath:"song/2.mp3",coverPath:"covers/1.jpg"},
-    {songName:"Salam-e-Ishq",filePath:"song/3.mp3",coverPath:"covers/1.jpg"},
-    {songName:"Salam-e-Ishq",filePath:"song/4.mp3",coverPath:"covers/1.jpg"},
-    {songName:"Salam-e-Ishq",filePath:"song/5.mp3",coverPath:"covers/1.jpg"},
-    {songName:"Salam-e-Ishq",filePath:"song/6.mp3",coverPath:"covers/1.jpg"},
+    {songName:"Gana Hi to hai",filePath:"song/2.mp3",coverPath:"covers/2.jpg"},
+    {songName:"Bhula ",filePath:"song/3.mp3",coverPath:"covers/3.jpg"},
+    {songName:"Mujhe ye",filePath:"song/4.mp3",coverPath:"covers/4.jpg"},
+    {songName:"alvida",filePath:"song/5.mp3",coverPath:"covers/5.jpg"},
+    {songName:"mai kya karu ji",filePath:"song/6.mp3",coverPath:"covers/6.jpg"},
+    {songName:"ho ho ",filePath:"song/7.mp3",coverPath:"covers/7.jpg"},
+    {songName:"chal hat",filePath:"song/8.mp3",coverPath:"covers/8.jpg"},
+    {songName:"dhoom ",filePath:"song/9.mp3",coverPath:"covers/9.jpg"},
+    {songName:"krish ",filePath:"song/10.mp3",coverPath:"covers/10.jpg"},
 ]
+
+
+songItems.forEach((element)=>{
+  element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+  element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+})
 
 masterPlay.addEventListener('click',()=>{
   if(audioElement.paused || audioElement.currentTime<=0)
@@ -40,4 +51,18 @@ audioElement.addEventListener('timeupdate',()=>{
 myProgessBar.addEventListener('change',()=>{
     audioElement.currentTime = myProgessBar.value  *  audioElement.duration/100;
 })
+const makeAllPlays = ()=>{
+  e.target.classList.add('fa-pause-circle');
+  Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    
+  })
 
+}
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+  element.addEventListener('click',(e)=>{
+    makeAllPlays();
+    e.target.classList.remove('fa-play-circle');
+    e.target.classList.add('fa-pause-circle');
+
+  })
+})
